cdded378a5f8ec46ffd5cbd09504b443de3cbff5
Leon <iamleonkigen@gmail.com>
2023-10-24 13:51:00 +0300

    completed assignmnet

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+let thankYouMessages = [];
+
+function writeCards(names, event_name){
+
+    thankYouMessages = [];
+
+    for(let count = 0; count < names.length; count++){
+        thankYouMessages.push(`Thank you, ${names[count]}, for the wonderful ${event_name} gift!`);
+    }
+    return thankYouMessages;
+}
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+}
+
+countDown(10);
+writeCards(names, "surprise");
